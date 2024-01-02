da153a0c1ec8171581b5137cb636147fe57f312c
m-sadegh-a <124788028+m-sadegh-a@users.noreply.github.com>
2024-01-02 20:03:20 +0330

    Create onEvent function to react to user events without implementation of events

--- app/src/main/java/com/sadegh/calculator/presentation/main_screen/MainScreenViewModel.kt
+++ app/src/main/java/com/sadegh/calculator/presentation/main_screen/MainScreenViewModel.kt
@@ -13,4 +13,30 @@ class MainScreenViewModel : ViewModel() {
 
     private val _result= MutableStateFlow("")
     val result=_result.asStateFlow()
+
+    fun onEvent(event:UserEvent){
+
+        when(event){
+            UserEvent.OnButtonsExpansionButtonClick -> TODO()
+            UserEvent.OnClearButtonClick -> TODO()
+            UserEvent.OnDeleteButtonClick -> TODO()
+            UserEvent.OnDivisionButtonClick -> TODO()
+            UserEvent.OnEqualButtonClick -> TODO()
+            UserEvent.OnMinusButtonClick -> TODO()
+            UserEvent.OnMultiplicationButtonClick -> TODO()
+            UserEvent.OnNumber0ButtonClick -> TODO()
+            UserEvent.OnNumber1ButtonClick -> TODO()
+            UserEvent.OnNumber2ButtonClick -> TODO()
+            UserEvent.OnNumber3ButtonClick -> TODO()
+            UserEvent.OnNumber4ButtonClick -> TODO()
+            UserEvent.OnNumber5ButtonClick -> TODO()
+            UserEvent.OnNumber6ButtonClick -> TODO()
+            UserEvent.OnNumber7ButtonClick -> TODO()
+            UserEvent.OnNumber8ButtonClick -> TODO()
+            UserEvent.OnNumber9ButtonClick -> TODO()
+            UserEvent.OnPercentButtonClick -> TODO()
+            UserEvent.OnPlusButtonClick -> TODO()
+            UserEvent.OnPointButtonClick -> TODO()
+        }
+    }
 }

ec1a9591642cd14fe0638de4e16587b927561183
m-sadegh-a <124788028+m-sadegh-a@users.noreply.github.com>
2024-01-04 14:53:44 +0330

    Change something in UseEvent class

--- app/src/main/java/com/sadegh/calculator/presentation/main_screen/UserEvent.kt
+++ app/src/main/java/com/sadegh/calculator/presentation/main_screen/UserEvent.kt
@@ -5,23 +5,11 @@ sealed interface UserEvent {
     object OnClearButtonClick : UserEvent
     object OnPercentButtonClick : UserEvent
     object OnDeleteButtonClick : UserEvent
-    object OnDivisionButtonClick : UserEvent
-    object OnMultiplicationButtonClick : UserEvent
-    object OnMinusButtonClick : UserEvent
-    object OnPlusButtonClick : UserEvent
+    data class OnOperatorButtonClick(val symbol: String) : UserEvent
     object OnEqualButtonClick : UserEvent
-    object OnNumber0ButtonClick : UserEvent
-    object OnNumber1ButtonClick : UserEvent
-    object OnNumber2ButtonClick : UserEvent
-    object OnNumber3ButtonClick : UserEvent
-    object OnNumber4ButtonClick : UserEvent
-    object OnNumber5ButtonClick : UserEvent
-    object OnNumber6ButtonClick : UserEvent
-    object OnNumber7ButtonClick : UserEvent
-    object OnNumber8ButtonClick : UserEvent
-    object OnNumber9ButtonClick : UserEvent
-    object OnPointButtonClick:UserEvent
-    object OnButtonsExpansionButtonClick:UserEvent
+    data class OnNumberButtonClick(val number: Int) : UserEvent
+    object OnPointButtonClick : UserEvent
+    object OnButtonsExpansionButtonClick : UserEvent
 
 
 }

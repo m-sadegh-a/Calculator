69330ba6ad57af884d90b00c8a5ae9127046eef7
m-sadegh-a <124788028+m-sadegh-a@users.noreply.github.com>
2024-01-04 18:57:36 +0330

    Create CalculatorApp which inherits from Application class and annotate MainActivity class with @AndroidEntryPoint annotation

--- app/src/main/java/com/sadegh/calculator/presentation/MainActivity.kt
+++ app/src/main/java/com/sadegh/calculator/presentation/MainActivity.kt
@@ -5,8 +5,10 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.sadegh.calculator.presentation.main_screen.HomeScreen
 import com.sadegh.calculator.ui.theme.CalculatorTheme
+import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.flow
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)

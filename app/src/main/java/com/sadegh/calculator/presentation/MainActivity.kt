8d4128c4c92483b9fb55a8329e29ab6964d4c734
m-sadegh-a <124788028+m-sadegh-a@users.noreply.github.com>
2024-01-04 14:33:10 +0330

    Implementation of calculateResult() function to calculate result

--- app/src/main/java/com/sadegh/calculator/presentation/MainActivity.kt
+++ app/src/main/java/com/sadegh/calculator/presentation/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.sadegh.calculator.presentation.main_screen.HomeScreen
 import com.sadegh.calculator.ui.theme.CalculatorTheme
+import kotlinx.coroutines.flow.flow
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/sadegh/calculator/presentation/main_screen/MainScreenViewModel.kt
+++ app/src/main/java/com/sadegh/calculator/presentation/main_screen/MainScreenViewModel.kt
@@ -1,18 +1,29 @@
 package com.sadegh.calculator.presentation.main_screen
 
 import androidx.lifecycle.ViewModel
+import com.sadegh.calculator.homeScreen.Operator
+import com.sadegh.calculator.homeScreen.ResultType
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.map
 
 @HiltViewModel
 class MainScreenViewModel : ViewModel() {
 
-    private val _input = MutableStateFlow("")
+    private val _input = MutableStateFlow(emptyList<String>())
     val input = _input.asStateFlow()
 
     private val _result = MutableStateFlow("")
     val result = _result.asStateFlow()
+
+    private
+    val lastElement: String
+        get() = input.value.last()
+
+    private fun isResultUndefined() = result.value == ResultType.undefined
+    private fun isClean() = input.value == mutableListOf("0")
+    private fun isLastInputAnOperator() = lastElement in arrayOf("÷", "x", "-", "+")
 
     fun onEvent(event: UserEvent) {
 
@@ -42,7 +53,71 @@ class MainScreenViewModel : ViewModel() {
 
     private fun onClearButtonClick() {
 
-        _input.value = "0"
+        _input.value = mutableListOf("0")
         _result.value = ""
+    }
+
+    private fun calculateResult(): String {
+
+        if (isResultUndefined()) {
+            return result.value
+        }
+
+        if (isClean()) {
+            return ""
+        }
+
+        if (input.value.size == 1) {
+            return (input.value.single().toDouble() * 1).toString()
+        }
+
+        val operatorsSymbol = arrayOf(
+            arrayOf("÷", "x", "%"),
+            arrayOf("-", "+")
+        )
+
+        val newInput = input.value.dropLast(if (isLastInputAnOperator()) 1 else 0).toMutableList()
+
+        operatorsSymbol.forEach { operatorsWithSamePrecedence ->
+
+            var index = 0
+            while (index < newInput.size) {
+
+                val numberOrOperator = newInput[index]
+
+                if (
+                    numberOrOperator == operatorsWithSamePrecedence[0] ||
+                    numberOrOperator == operatorsWithSamePrecedence[1] ||
+                    numberOrOperator == operatorsWithSamePrecedence.getOrNull(2)
+                ) {
+
+                    val number1 = newInput[index - 1].toDouble()
+                    var number2 = newInput.getOrNull(index + 1)?.toDoubleOrNull()
+
+                    if (numberOrOperator == "%" && number2 == null) {
+                        newInput.add(index = index + 1, "1")
+                        number2 = 1.0
+                    }
+
+                    if (number2 == 0.0 && numberOrOperator == "÷") {
+                        return ResultType.undefined
+                    }
+
+                    val operator = Operator(numberOrOperator)
+                    val result = operator.operate(number1, number2!!).toString()
+
+                    newInput[index + 1] = result
+                    newInput.removeAt(index)
+                    newInput.removeAt(index - 1)
+
+                } else {
+
+                    index++
+
+                }
+            }
+        }
+
+        return newInput.single()
     }
 }

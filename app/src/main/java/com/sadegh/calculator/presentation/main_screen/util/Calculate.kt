8b6b6fa4fad39a6dc550051f3c213fd763af745c
m-sadegh-a <m.sadegh.amerehi@gmail.com>
2024-01-08 03:21:35 +0330

    Break calculateResult function into four functions

--- app/src/main/java/com/sadegh/calculator/presentation/main_screen/util/Calculate.kt
+++ app/src/main/java/com/sadegh/calculator/presentation/main_screen/util/Calculate.kt
@@ -1,71 +1,99 @@
 package com.sadegh.calculator.presentation.main_screen.util
 
-fun calculateResult(input: List<String>): String {
+fun calculateResult(inputs: List<String>): String {
 
-    if (input == listOf("0")) {
+    if (inputs == listOf("0")) {
         return ""
     }
 
-    if (input.size == 1) {
-        return (input.single().toDouble() * 1).toString()
+    if (inputs.size == 1) {
+        return (inputs.single().toDouble() * 1).toString()
     }
 
-    val operatorsSymbol = listOf(
-        listOf(Operator.MultiplyOperator, Operator.DivisionOperator, Operator.PercentageOperator),
-        listOf(Operator.AddOperator, Operator.SubtractOperator)
-    )
-
-    val inputWithoutLastOperator =
-        if (input.last() in listOf("÷", "x", "-", "+", "%")) {
-            input.dropLast(1)
+    val inputsWithoutLastOperator =
+        if (inputs.last() in listOf("÷", "x", "-", "+")) {
+            inputs.dropLast(1)
         } else {
-            input
+            inputs
         }
             .toMutableList()
 
-    operatorsSymbol.forEach { operatorsWithSamePrecedence ->
+    val operatorsPriorities = Operator.priorities
 
-        var index = 0
-        while (index < inputWithoutLastOperator.size) {
+    return calculateResult(inputsWithoutLastOperator, operatorsPriorities)
 
-            val numberOrOperator = inputWithoutLastOperator[index]
+}
 
-            if (
-                numberOrOperator == operatorsWithSamePrecedence[0].symbol ||
-                numberOrOperator == operatorsWithSamePrecedence[1].symbol ||
-                numberOrOperator == operatorsWithSamePrecedence.getOrNull(2)?.symbol
-            ) {
+fun calculateResult(
+    inputs: MutableList<String>,
+    operatorsPriorities: List<Int>,
+): String {
 
-                val number1 = inputWithoutLastOperator[index - 1].toDouble()
-                var number2 = inputWithoutLastOperator.getOrNull(index + 1)?.toDoubleOrNull()
+    try {
 
-                if (numberOrOperator == "%" && number2 == null) {
-                    inputWithoutLastOperator.add(index = index + 1, "1")
-                    number2 = 1.0
-                }
+        operatorsPriorities.forEach { operatorPriority ->
 
-                if (number2 == 0.0 && numberOrOperator == "÷") {
-                    return ResultType.UNDEFINED
-                }
+            inputs applyOperatorsWithPriority operatorPriority
 
-                val operator = Operator.getOperatorFromSymbolOrNull(numberOrOperator)
-                operator?.let {
-
-                    val result = operator(number1, number2!!).toString()
-
-                    inputWithoutLastOperator[index + 1] = result
-
-                    inputWithoutLastOperator.removeAt(index)
-
-                    inputWithoutLastOperator.removeAt(index - 1)
-                }
-            } else {
-
-                index++
-
-            }
         }
+
+    } catch (e: ArithmeticException) {
+        return e.message!!
     }
 
-    return inputWithoutLastOperator.single()
+    return inputs.single()
+
+}
+
+infix fun MutableList<String>.applyOperatorsWithPriority(priority: Int) {
+
+    var index = 0
+
+    while (index <= this.lastIndex) {
+
+        val input = this[index]
+
+        //operator is null when it is not equal to an operator in Operator class
+        val operator = Operator.getOperatorFromSymbolOrNull(input)
+
+        if (operator != null && operator.priority == priority) {
+
+            if (operator == Operator.PercentageOperator) {
+
+                //nextInput is null when the percentage is the last element in inputs
+                val nextInput = this.getOrNull(index + 1)
+
+                /*
+                check if there is another operator immediately after the percentage
+                or if the percentage is the last input
+                 */
+                if (nextInput == null || nextInput in Operator.symbols) {
+                    this.add(index + 1, "1")
+                }
+            }
+
+            val number1 = this[index - 1].toDouble()
+            val number2 = this[index + 1].toDouble()
+
+            val result = operate(number1, number2, operator)
+
+            this[index + 1] = result
+            this.removeAt(index)
+            this.removeAt(index - 1)
+
+            index--
+        }
+
+        index++
+    }
+}
+
+fun operate(number1: Double, number2: Double, operator: Operator): String {
+
+    if (number2 == 0.0 && operator == Operator.DivisionOperator) {
+        throw ArithmeticException(ResultType.UNDEFINED)
+    }
+
+    return operator(number1, number2).toString()
+
 }

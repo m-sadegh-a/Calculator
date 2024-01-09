78e30f710e3efc17bb86bf8382302c3e58d3e293
m-sadegh-a <m.sadegh.amerehi@gmail.com>
2024-01-10 00:43:11 +0330

    Convert Input interface to class to make it simpler

--- app/src/main/java/com/sadegh/calculator/presentation/main_screen/util/Input.kt
+++ app/src/main/java/com/sadegh/calculator/presentation/main_screen/util/Input.kt
@@ -1,99 +1,65 @@
 package com.sadegh.calculator.presentation.main_screen.util
 
-sealed interface Input {
+sealed class Input(private val displayValue: String) {
 
-    sealed class OperatorInput(val priority: Int) : Input {
+    sealed class OperatorInput(val priority: Int, symbol: String) : Input(symbol) {
+
         abstract operator fun invoke(number1: Double, number2: Double): Double
 
-        sealed class BasicOperatorInput(priority: Int) : OperatorInput(priority) {
+        sealed class BasicOperatorInput(priority: Int, symbol: String) :
+            OperatorInput(priority, symbol) {
 
-            object AddOperatorInput : BasicOperatorInput(5) {
+            object AddOperatorInput : BasicOperatorInput(5, "+") {
                 override operator fun invoke(number1: Double, number2: Double): Double {
                     return number1 + number2
                 }
-
-                override fun toString() = "+"
-
             }
 
-            object SubtractOperatorInput : BasicOperatorInput(5) {
+            object SubtractOperatorInput : BasicOperatorInput(5, "-") {
                 override operator fun invoke(number1: Double, number2: Double): Double {
                     return number1 - number2
                 }
-
-                override fun toString() = "-"
-
             }
 
-            object DivisionOperatorInput : BasicOperatorInput(4) {
+            object DivisionOperatorInput : BasicOperatorInput(4, "÷") {
                 override operator fun invoke(number1: Double, number2: Double): Double {
                     return number1 / number2
                 }
-
-                override fun toString() = "÷"
-
             }
 
-            object MultiplyOperatorInput : BasicOperatorInput(4) {
+            object MultiplyOperatorInput : BasicOperatorInput(4, "x") {
                 override operator fun invoke(number1: Double, number2: Double): Double {
                     return number1 * number2
                 }
-
-                override fun toString() = "x"
-
             }
         }
 
-        object PercentageOperatorInput : OperatorInput(1) {
+        object PercentageOperatorInput : OperatorInput(1, "%") {
 
             override operator fun invoke(number1: Double, number2: Double): Double {
                 return (number1 * number2) / 100
             }
-
-            override fun toString() = "%"
-
         }
 
         companion object {
 
             val priorities = listOf(1, 4, 5)
-
-            fun getOperatorFromSymbolOrNull(symbol: String): OperatorInput? {
-
-                return when (symbol) {
-
-                    "+" -> BasicOperatorInput.AddOperatorInput
-
-                    "-" -> BasicOperatorInput.SubtractOperatorInput
-
-                    "x" -> BasicOperatorInput.MultiplyOperatorInput
-
-                    "%" -> PercentageOperatorInput
-
-                    "÷" -> BasicOperatorInput.DivisionOperatorInput
-
-                    else -> null
-                }
-            }
         }
     }
 
-    open class Number (private val stringValue: String) : Input {
+    open class Number(stringValue: String) : Input(stringValue) {
         object NeperNumber : Number("e")
 
         val doubleValue: Double
-            get() = if (stringValue == "e") {
+            get() = if (toString() == "e") {
                 Math.E
             } else {
-                stringValue.toDouble()
+                toString().toDouble()
             }
 
-        override fun toString() = stringValue
-
     }
 
-    object Equal : Input {
-        override fun toString() = "="
+    object Equal : Input("=")
 
-    }
+    override fun toString() = displayValue
 }
